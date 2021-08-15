8af5b1ef2c0ff4c4d74cce7f8cb06c3f0b027939
Maigret Aurélien <github@dewep.net>
2021-08-15 21:01:20 +0200

    Update formations

--- dewep/formations.js
+++ dewep/formations.js
@@ -3,13 +3,18 @@
 
   const formationSlugs = [
     'breedingOwnStar',
-    // 'breedingP89Star',
+    'breedingP89Star',
     // 'defenderBase',
     // 'defenderOwnStar',
     // 'defenderP89Star',
-    // 'outpost',
+    'outpost',
     // 'baseAttack',
+    // 'attackFromBehind',
+    'baiter',
   ]
+  const avoidArea =
+    null
+    // [...star_p89.position, 800]
 
   const formationSlug = formationSlugs.join(',')
   const isTopSide = base.position[0] === 1600
@@ -48,6 +53,12 @@
     ],
     baseAttack: [
       { id: 'attacker0', size: 3, pos: enemy_base.position, type: 'attacker', attack: 'base', energyPos: getChainPosition(star_p89.position, 1, 3) }
+    ],
+    attackFromBehind: [
+      { id: 'attackerBehind0', size: 1, pos: enemy_base.position, type: 'attacker', attack: 'base', energyPos: [myStar.position[0] - sideModifier * 20, sideModifier * 50 + myStar.position[1]], avoidArea: [...star_p89.position, 800] }
+    ],
+    baiter: [
+      { id: 'baiter0', size: 1, pos: enemy_base.position, type: 'baiter', energyPos: [myStar.position[0] - sideModifier * 20, sideModifier * 50 + myStar.position[1]] }
     ]
   }
 
@@ -56,10 +67,12 @@
     formation.push(...formations[slug])
   }
   const workerSize = formation.reduce((a, b) => a + b.size, 0)
+  let removedWorkers = {}
 
   // memory.workers = {}
   if (memory.lastFormation !== formationSlug) {
     memory.lastFormation = formationSlug
+    removedWorkers = memory.workers || {}
     memory.workers = {}
   }
 
@@ -76,23 +89,36 @@
       }
     }
   }
+  const aliveEnemies = Object.keys(spirits).filter(name => !name.includes('Dewep')).map(name => spirits[name]).filter(spirit => spirit.hp)
 
   for (let index = 0; index < myAliveSpirits.length; index++) {
     const spirit = myAliveSpirits[index]
     let workerId = memory.workers[spirit.id]
 
     if (!workerId) {
-      const ratios = formation.map(w => (workerCounters[w.id] || 0) / w.size)
-      const workerIndex = ratios.findIndex(ratio => ratio === Math.min(...ratios))
-      workerId = formation[workerIndex].id
-      memory.workers[spirit.id] = workerId
-      workerCounters[workerId] = (workerCounters[workerId] || 0) + 1
+      while (true) {
+        const ratios = formation.map(w => (workerCounters[w.id] || 0) / w.size)
+        const workerIndex = ratios.findIndex(ratio => ratio === Math.min(...ratios))
+        workerId = formation[workerIndex].id
+        if (removedWorkers[spirit.id] && removedWorkers[spirit.id] !== workerId) {
+          const spiritId = Object.keys(removedWorkers).find(id => removedWorkers[id] === workerId)
+          if (spiritId) {
+            memory.workers[spiritId] = workerId
+            workerCounters[workerId] = (workerCounters[workerId] || 0) + 1
+            continue
+          }
+        }
+        memory.workers[spirit.id] = workerId
+        workerCounters[workerId] = (workerCounters[workerId] || 0) + 1
+        spirit.set_mark('null')
+        break
+      }
     }
 
     spirit.shout(workerId)
     const worker = formation.find(f => f.id === workerId)
 
-    const enemies = spirit.sight.enemies.map(id => ({ spirit: spirits[id], dist: dist(spirits[id].position, spirit.position) })).filter(e => e.spirit.energy >= 0 && e.dist <= 200)
+    const enemies = spirit.sight.enemies.map(id => ({ spirit: spirits[id], dist: dist(spirits[id].position, spirit.position) })).filter(e => e.spirit.energy >= 0)
     enemies.sort((a, b) => a.spirit.energy - b.spirit.energy)
 
     if (worker.type === 'chain') {
@@ -112,9 +138,9 @@
           energize(spirit, spirit, true)
         }
       }
-      spirit.move(worker.pos.map(val => val - 10 + Math.round(Math.random() * 20)))
+      move(spirit, worker.pos, 10, worker.avoidArea || avoidArea)
     } else if (worker.type === 'defender') {
-      spirit.move(worker.pos.map(val => val - 10 + Math.round(Math.random() * 20)))
+      move(spirit, worker.pos, 10, worker.avoidArea || avoidArea)
     } else if (worker.type === 'attacker') {
       if (spirit.energy_rate < 0.3) {
         spirit.set_mark('reload')
@@ -122,20 +148,37 @@
         spirit.set_mark('full')
       }
       if (spirit.mark === 'reload') {
-        spirit.move(worker.energyPos)
-        if (spirit.sight.structures.filter(s => s.startsWith('star_')).length) {
+        move(spirit, worker.energyPos, 10, worker.avoidArea || avoidArea)
+        if (spirit.sight.structures.filter(s => s.startsWith('star_')).length || spirit.sight.structures.includes(base.id)) {
           energize(spirit, spirit, true)
         }
       } else {
-        spirit.move(worker.pos)
+        move(spirit, worker.pos, 10, worker.avoidArea || avoidArea)
         if (worker.attack === 'outpost') {
           energize(spirit, outpost, false)
         } else {
           energize(spirit, enemy_base, false)
         }
       }
+    } else if (worker.type === 'baiter') {
+      enemies.sort((a, b) => a.spirit.dist - b.spirit.dist)
+      const baiterAvoidArea = enemies.length ? [...enemies[0].spirit.position, 350] : (worker.avoidArea || avoidArea)
+      if (spirit.energy_rate < 0.3) {
+        spirit.set_mark('reload')
+      } else if (spirit.energy_rate === 1) {
+        spirit.set_mark('full')
+      }
+      if (spirit.mark === 'reload') {
+        move(spirit, worker.energyPos, 10, baiterAvoidArea)
+        if (spirit.sight.structures.filter(s => s.startsWith('star_')).length || spirit.sight.structures.includes(base.id)) {
+          energize(spirit, spirit, true)
+        }
+      } else {
+        move(spirit, enemies.length >= 5 ? (isTopSide ? [4200, 2400] : [0, 0]) : aliveEnemies.length ? aliveEnemies[Math.floor(Math.random() * aliveEnemies.length)].position : enemy_base.position, 0, baiterAvoidArea)
+      }
     }
 
+    enemies.filter(e => e.dist <= 200)
     if (enemies.length) {
       energize(spirit, enemies[0].spirit, false)
     }
@@ -157,4 +200,46 @@ function energize (from, to, friend = true) {
   from.energize(to)
   to.energy += (friend ? 1 : -1) * from.size
   to.energy_rate = to.energy / to.energy_capacity
+}
+
+function move (spirit, to, areaRandom = 0, avoidArea) {
+  let pos = [...to].map(val => val - areaRandom + Math.round(Math.random() * areaRandom * 2))
+  if (avoidArea) {
+    pos = getSafePointAround([avoidArea[0], avoidArea[1]], avoidArea[2], spirit.position, pos)
+  }
+  spirit.move(pos)
+}
+
+function getSafePointAround(obj, r, from, to) {
+  r += r * 0.1
+  xA = obj[0]; yA = obj[1]
+  xB = from[0]; yB = from[1]
+  
+  const R = Math.sqrt(Math.pow(dist(from, obj), 2) + r*r)
+  const a = 2 * (xB - xA)
+  const b = 2 * (yB - yA)
+  const c = (xB - xA) * (xB - xA) + (yB - yA) * (yB - yA) - R*R + r*r
+  const delta = (2*a*c)*(2*a*c) - 4 *(a*a + b*b) * (c*c - (b*b*r*r))
+
+  // No hit, safe passage
+  if (delta < 0) {
+    return to
+  }
+
+  const x1 = Math.round(xA + (2*a*c - Math.sqrt(delta)) / (2*(a*a+b*b)))
+  const x2 = Math.round(xA + (2*a*c + Math.sqrt(delta)) / (2*(a*a+b*b)))
+
+  let y1, y2;
+  if (b != 0) {
+    y1 = Math.round(yA + (c-a * (x1 - xA)) / b)
+    y2 = Math.round(yA + (c-a * (x2 - xA)) / b)
+  } else {
+    const tmp = Math.sqrt(R*R - Math.pow(((2*c - a*a)/(2*a))))
+    y1 = Math.round(yA + b/2 + tmp)
+    y2 = Math.round(yA + b/2 - tmp)
+  }
+
+  if (dist(to, [x1, y1]) < dist(to, [x2, y2]))
+    return [x1, y1]
+  return [x2, y2]
 }
